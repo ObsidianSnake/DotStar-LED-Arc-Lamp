a4223c41634c436853b933b2e8c3d98f2f0bad89
ObsidianSnake <Nlamica@msn.com>
2022-01-06 16:28:24 -0800

    Update updateStrips.ino

--- updateStrips.ino
+++ updateStrips.ino
@@ -1,14 +1,12 @@
 void updateStrips() {
-
-  // first, clearing the strips
-  for (int k = 0 ; k < NUMPIXELS ; k++) {
-    strip.setPixelColor(k, (int)segment_brightness );
-  }
-
-  for (int k = 0 ; k < NUMPIXELS ; k++) {
-    if ( ( k >= ((int)segment_middle - (int)segment_ss_width) ) && ( k <= ((int)segment_middle + (int)segment_ss_width)  ) ) {      
-        strip.setPixelColor(k, ((long)segment_brightness) << 24 );
-        strip.setPixelColor(k, ((long)segment_brightness) << 24 );
+  for (int k = 0 ; k < NUMPIXELS ; k++) {    
+    if ( ( k >= ((int)segment_middle - (int)segment_ss_width) ) && ( k <= ((int)segment_middle + (int)segment_ss_width)  ) )
+    {      
+        strip.setPixelColor(k, segment_brightness);
+    }
+    else
+    {
+      strip.setPixelColor(k, 0);
     }
   }
 

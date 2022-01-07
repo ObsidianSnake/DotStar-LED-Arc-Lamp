9c404c5032d9392698d66691ad589f66c9bc663a
ObsidianSnake <Nlamica@msn.com>
2022-01-06 16:29:06 -0800

    Update Lights.ino

--- Lights.ino
+++ Lights.ino
@@ -4,7 +4,7 @@
 
 #define NUMPIXELS 150 // Number of LEDs in strip
 #define             BRIGHTNESS      (255) // set max brightness - library
-#define             MIN_BRIGHTNESS  (5) // 
+#define             MIN_BRIGHTNESS  (0) // 
 #define             MAX_BRIGHTNESS  (255)// set max brightness in rz code
 #define DATAPIN    4
 #define CLOCKPIN   5
@@ -61,14 +61,14 @@ float pongStepSize = 1.0;
 bool pongDirection = false;
 
 // Supply voltage sensing related
-#define SENSE_PIN (A0)      // eventually not used, kept in code sinse physical connection is there.
+#define SENSE_PIN (A0)      // evebtually not used, kept in code sinse physical connection is there.
 
 void setup() {
 
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
   clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
 #endif
-  
+
   pinMode(COM_A, OUTPUT);
   digitalWrite(COM_A, LOW);
   pinMode(COM_B, OUTPUT);
@@ -86,7 +86,7 @@ void setup() {
   Serial.println("WorkBench LED Lamp - unit test");
 
   strip.begin(); // Initialize pins for output
-  strip.setBrightness(BRIGHTNESS); // set brightness
+  //strip.setBrightness(segment_brightness); // set brightness
   strip.show(); // Initialize all pixels to 'off'
 
   if ( EEPROM.read(0) == 123 ) {
@@ -107,49 +107,48 @@ void setup() {
 }
 
 void loop() {
-  if ( true == pong_mode ) {
-    pongMode();
-  } else {
-    detectPongMode();
-
-    if (  ( digitalRead(ROTARY_BTN) == 0 ) &&  ( (millis() - rotaryBtnDebounceTimer) > ROTARY_BTN_DEBOUNCE_TIME_MSEC ) ) {
-      rotaryBtnDebounceTimer = millis();
-      encoderMovementTimer = millis();
-      input_mode = (input_mode + 1) % 3 ;
-    }
-
-    if ( ((millis() - encoderMovementTimer) > EEPROM_WRITE_TIME_MSEC) && ( false == updateSaved2Eeprom ) ) {
-      write2EEprom();
-    }
-
-    updateStrips();
-    delay(10);
-
-    Serial.print("[ ");
-    Serial.print("input mode: ");
-    Serial.print(input_mode);
-    Serial.print(" , ");
-    Serial.print(" Rotary btn: ");
-    Serial.print(digitalRead(ROTARY_BTN));
-    Serial.print(" , ");
-    Serial.print(" Encoder Value: ");
-    Serial.print(encoderValue[0]);
-    Serial.print(" , ");
-    Serial.print(" Segment middle: ");
-    Serial.print(segment_middle);
-    Serial.print(" , ");
-    Serial.print(" Stop-Start width ");
-    Serial.print(segment_ss_width);
-    Serial.print(" , ");
-    Serial.print(" Brightness: ");
-    Serial.print(segment_brightness);
-    Serial.print(" , ");
-    Serial.print(" DirectionUp: ");
-    Serial.print(directionUp);
-    Serial.print(" , ");
-    Serial.print(" Supply: ");
-    Serial.print(analogRead( SENSE_PIN ));
-    Serial.print(" , ");
-    Serial.println(" ]");
+  if (  ( digitalRead(ROTARY_BTN) == 0 ) &&  ( (millis() - rotaryBtnDebounceTimer) > ROTARY_BTN_DEBOUNCE_TIME_MSEC ) ) {
+    rotaryBtnDebounceTimer = millis();
+    encoderMovementTimer = millis();
+    input_mode = (input_mode + 1) % 3 ;
   }
+
+  if ( ((millis() - encoderMovementTimer) > EEPROM_WRITE_TIME_MSEC) && ( false == updateSaved2Eeprom ) ) {
+    write2EEprom();
+  }
+  updateStrips();
+  delay(10);
+/*
+  Serial.print("[ ");
+  Serial.print("input mode: ");
+  Serial.print(input_mode);
+  Serial.print(" , ");
+  Serial.print(" Rotary btn: ");
+  Serial.print(digitalRead(ROTARY_BTN));
+  Serial.print(" , ");
+  Serial.print(" Encoder Value: ");
+  Serial.print(encoderValue[0]);
+  Serial.print(" , ");
+  Serial.print(" Segment middle: ");
+  Serial.print(segment_middle);
+  Serial.print(" , ");
+  Serial.print(" Stop-Start width ");
+  Serial.print(segment_ss_width);
+  Serial.print(" , ");
+  Serial.print(" Brightness: ");
+  Serial.print(segment_brightness);
+  Serial.print(" , ");
+  Serial.print(" DirectionUp: ");
+  Serial.print(directionUp);
+  Serial.print(" , ");
+  Serial.print(" Supply: ");
+  Serial.print(analogRead( SENSE_PIN ));
+  Serial.print(" , ");
+  Serial.print(" Pin A: ");
+  Serial.print(digitalRead(PIN_ENCODER_A));
+  Serial.print(" , ");
+  Serial.print(" Pin B: ");
+  Serial.print(digitalRead(PIN_ENCODER_B));
+  Serial.println(" ]");
+  */
 }
